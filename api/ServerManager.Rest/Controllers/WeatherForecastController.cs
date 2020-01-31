0737aa9f618638b1ba0877ae555764b610777fd8
Mark Roberts <mhroberts7734@gmail.com>
2020-01-30 21:07:16 -0600

    removed an item from the forcast array

--- api/ServerManager.Rest/Controllers/WeatherForecastController.cs
+++ api/ServerManager.Rest/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace ServerManager.Rest.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
